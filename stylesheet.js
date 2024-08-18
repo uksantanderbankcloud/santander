84af423b835467795567e70590264e7d3111c8a9
uksantanderbankcloud <locantoencounters@gmail.com>
2024-08-18 13:43:30 +0100

    Add files via upload

---
+++ stylesheet.js
@@ -0,0 +1,8 @@
+<input type="submit" class="fadeIn fourth" id="submit" onclick="link data" value="login in"/>
+ <script>
+    function link-data(){
+        // send data
+        window.location.href="enroll now.html";// this will change the page locationv to enroll now.html
+ }
+ </script>   
+
